b56150de0648880174bc4e1f33a41adaf45d40f1
bendes-ai <qdvjn4kcys@privaterelay.appleid.com>
2026-07-01 20:41:46 +0200

    Update App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Layout from './components/Layout.jsx';
-
 import Startseite from './pages/Startseite.jsx';
 import Lerntechniken from './pages/Lerntechniken.jsx';
 import LernApp from './pages/LernApp.jsx';
